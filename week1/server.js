9963b2aa5264bff87880d6ee31dffd8756012bee
Shurui Liu <shurui91@gmail.com>
2017-02-26 21:20:55 -0800

    basic node server setup is ready

--- week1/server.js
+++ week1/server.js
@@ -1,7 +1,26 @@
-var http = require('http');
+console.log("nodejs test");
 
-http.createServer(function (request, response) {
-	response.writeHead(200, {"Content-Type" : "text/plain"});
-	response.write("Hello World");
-	response.end;
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+	if (req.url === '/') {
+		res.writeHead(200, {"Content-Type" : "text-html"});
+		var html = fs.readFileSync(__dirname + "/index.html");
+		res.end(html);
+	}
+	if (req.url === '/api') {
+		res.writeHead(200, {"Content-Type" : "application/json"});
+		var obj = {
+			name: 'Shurui Liu',
+			age: 12
+		};
+		res.end(JSON.stringify(obj));
+	}
 }).listen(3000);
+
+/*
+http.createServer(function (req, res) {
+	
+}).listen(3000);
+*/
