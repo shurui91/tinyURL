e1e07ddfbe03893e76b24c716f988253ab103341
Shurui Liu <shurui91@gmail.com>
2017-07-12 10:38:10 -0700

    start to fix tinyURL project

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
-var express = require('express');
-var app = express();
-// var restRouter = require('./routes/rest');
-var apiRouter = require('./routes/api');
-var redirectRouter = require('./routes/redirect');
-var indexRouter = require('./routes/index');
-
-app.longToShortHash = {};
-app.shortToLongHash = {};
-
-app.use('/public', express.static(__dirname + '/public'));
-
-app.use('/api/v1', apiRouter);
-
-// comma means shortUrl is a variable name
-app.use('/:shortUrl', redirectRouter);
-
-app.use('/', indexRouter);
-
-app.listen(3000);
+var http = require('http');
+http.createServer(function (req, res) {
+	res.writeHead(200, {"Content-Type" : "text-plain"});
+	res.write("Hello there!");
+	res.end();
+}).listen(3000);
