11f117ac864f21814d09a404631f5690c9c07fe3
Shurui Liu <shurui91@gmail.com>
2017-10-06 11:13:42 -0700

    change success to then, for http rules, this is for ng1.6+

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,12 +1,18 @@
 var app = angular.module('tinyUrlApp');
 
-app.controller('homeController', ['$scope', '$http', '$location', function($scope, $http, $location) {
-		$scope.submit = function () {
-			$http.post("/api/v1/urls", {
-				longUrl : $scope.longUrl
-			})
-			.success(function (data) {
-				$location.path("/urls/" + data.shortUrl);
-			})
-		}
-}]);
+app.controller('homeController', [
+	'$scope',
+	'$http',
+	'$location',
+	function($scope, $http, $location) {
+		$scope.submit = function() {
+			$http
+				.post('/api/v1/urls', {
+					longUrl: $scope.longUrl
+				})
+				.then(function(data) {
+					$location.path('/urls/' + data.shortUrl);
+				});
+		};
+	}
+]);
