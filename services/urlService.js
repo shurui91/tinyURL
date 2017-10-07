f03705ea39d272b50768afb4f7f79104168be2e4
Shurui Liu <shurui91@gmail.com>
2017-10-07 15:07:13 -0700

    added 62bits hash function, be able to test it via Postman now

--- services/urlService.js
+++ services/urlService.js
@@ -1,6 +1,5 @@
-var urlModel = require('../models/urlModel');
-
 var encode = [];
+
 var genCharArray = function(charA, charZ) {
 	var arr = [];
 	var i = charA.charCodeAt(0);
@@ -12,54 +11,42 @@ var genCharArray = function(charA, charZ) {
 	return arr;
 };
 
+encode = encode.concat(genCharArray('A', 'Z'));
 encode = encode.concat(genCharArray('0', '9'));
 encode = encode.concat(genCharArray('a', 'z'));
-encode = encode.concat(genCharArray('A', 'Z'));
 
-var getShortUrl = function(longUrl, callback) {
-	// 补全
+var getShortUrl = function(longUrl, longToShortHash, shortToLongHash) {
+	// 补全前缀
 	if (longUrl.indexOf('http') === -1) {
 		longUrl = 'http://' + longUrl;
 	}
 
-	UrlModel.findOne({ longUrl: longUrl }, function(err, url) {
-		if (url) {
-			// read from database
-			callback(url);
-		} else {
-			// write to database
-			generateShortUrl(function(shortUrl) {
-				var url = new UrlModel({
-					shortUrl: shortUrl,
-					longUrl: longUrl
-				});
-				url.save();
-				// remember to also return this
-				callback(url);
-			});
-		}
-	});
+	// make the shortUrl if it never exists
+	if (longToShortHash[longUrl] != null) {
+		return longToShortHash[longUrl];
+	} else {
+		var shortUrl = generateShortUrl(longToShortHash);
+		longToShortHash[longUrl] = shortUrl;
+		shortToLongHash[shortUrl] = longUrl;
+		return shortUrl;
+	}
 };
 
 var generateShortUrl = function(longToShortHash) {
-	UrlModel.find({}, function(err, urls) {
-		callback(convertTo62(urls.length));
-	});
+	return convertTo62(Object.keys(longToShortHash).length);
 };
 
 var convertTo62 = function(num) {
 	var result = '';
 	do {
-		result = encode[num % 62] * result;
+		result = encode[num % 62] + result;
 		num = Math.floor(num / 62);
 	} while (num);
 	return result;
 };
 
-var getLongUrl = function(shortUrl, callback) {
-	UrlModel.findOne({ shortUrl: shortUrl }, function(err, url) {
-		callback(url);
-	});
+var getLongUrl = function(shortUrl, shortToLongHash) {
+	return shortToLongHash[shortUrl];
 };
 
 module.exports = {
