6ec3da3197ab830ef69a905ebbd7449c29d5bc3c
Shurui Liu <shurui91@gmail.com>
2017-10-09 14:29:02 -0700

    start to connect to mongoDB

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://user:user@ds125060.mlab.com:25060/tinyurl');
 
-app.longToShortHash = {};
-app.shortToLongHash = {};
+// app.longToShortHash = {};
+// app.shortToLongHash = {};
 
 app.use('/public', express.static(__dirname + '/public'));
 
