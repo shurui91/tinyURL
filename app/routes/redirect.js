9e53e8d0b2877edb3fbc91631ebe22389e6de11a
Shurui Liu <shurui91@gmail.com>
2017-10-19 15:28:04 -0700

    path required bug fixed

--- app/routes/redirect.js
+++ app/routes/redirect.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var urlService = require('../services/urlService');
+var path = require('path');
 
 router.get('*', function(req, res) {
 	// slice(1) removes the starting slash
