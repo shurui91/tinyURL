031617453e7f0e658447180f2c7c41234abf8da7
Shurui Liu <shurui91@gmail.com>
2017-10-21 15:27:02 -0700

    urlController should be done by now

--- app/public/js/controllers/urlController.js
+++ app/public/js/controllers/urlController.js
@@ -5,18 +5,66 @@ app.controller('urlController', [
 	'$http',
 	'$routeParams',
 	function($scope, $http, $routeParams) {
+		$http.get('/api/v1/urls/' + $routeParams.shortUrl).then(function(data) {
+			$scope.shortUrl = data.shortUrl;
+			$scope.longUrl = data.longUrl;
+			$scope.shortUrlToShow = 'http://localhost/' + data.shortUrl;
+		});
 		$http
-			.get('/api/v1/urls/' + $routeParams.shortUrl)
-			.then(function(response) {
-				$scope.shortUrl = response.data.shortUrl;
-				$scope.longUrl = response.data.longUrl;
-				$scope.shortUrlToShow =
-					'http://localhost/' + response.data.shortUrl;
-			});
-		$http
-			.get('/api/v1/urls' + $routeParams.shortUrl + 'totalClicks')
+			.get('/api/v1/urls/' + $routeParams.shortUrl + '/totalClicks')
 			.then(function(data) {
 				$scope.totalClicks = data;
 			});
+
+		$scope.hour = 'hour';
+		$scope.day = 'day';
+		$scope.month = 'month';
+
+		$scope.getTime = function(time) {
+			$scope.lineLabels = [];
+			$scope.lineData = [];
+			$scope.time = time;
+			$http
+				.get('/api/v1/urls/' + $routeParams.shortUrl + '/' + time)
+				.then(function(data) {
+					data.forEach(function(info) {
+						var legend = '';
+						if (time === 'hour') {
+							if (info._id.minutes < 10) {
+								info._id.minutes = '0' + info._id.minutes;
+							}
+							legend = info._id.hour + ':' + info._id.minutes;
+						}
+						if (time === 'day') {
+							legend = info._id.hour + ':00';
+						}
+						if (time === 'month') {
+							legend = info._id.month + '/' + info._id.day;
+						}
+
+						$scope['lineLabels'].push(legend);
+						$scope['lineData'].push(info.count);
+					});
+				});
+		};
+
+		$scope.getTime('hour');
+
+		var renderChart = function(chart, infos) {
+			$scope[chart + 'Labels'] = [];
+			$scope[chart + 'Data'] = [];
+			$http
+				.get('/api/v1/urls/' + $routeParams.shortUrl + '/' + infos)
+				.then(function(data) {
+					data.forEach(function(info) {
+						$scope[chart + 'Labels'].push(info._id);
+						$scope[chart + 'Data'].push(info.count);
+					});
+				});
+		};
+		renderChart('pie', 'referrer');
+		renderChart('doughnut', 'country');
+		renderChart('bar', 'platform');
+		renderChart('base', 'browser');
 	}
 ]);
